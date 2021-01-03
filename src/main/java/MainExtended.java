b23f5445f737f2f11f43de57de7c66ebfb62344b
Ewa Dzioba <ewa.dzioba@gmail.com>
2021-01-03 14:58:08 +0100

    Initial version

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String mySurname = "Grażyna";
+        int myShoe = 38;
+        double age = 35.5;
+        char favouriteLetter = 'E';
+        boolean isHappy = true;
+        String myGreetings = "Hello from FirstClass!";
+        System.out.println(mySurname);
+        System.out.println(myShoe);
+        System.out.println(age);
+        System.out.println(myGreetings);
+        System.out.println(favouriteLetter);
+        System.out.println(isHappy);
+    }
+}
---
+++ src/main/java/SecondClass.java
@@ -0,0 +1,14 @@
+public class SecondClass {
+    public static void main(String[] args) {
+        String myName = "Ewa";
+        int myAge = 36;
+        double height = 1.62;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        String myGreetings = "Hello from FirstClass!";
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(myGreetings);
+    }
+}
