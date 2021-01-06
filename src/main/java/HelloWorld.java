752257f1b027e58272836568cccfb1a6a018e84f
Ewa Dzioba <ewa.dzioba@gmail.com>
2021-01-06 19:49:50 +0100

    Revert "“Added h1 tag”"
    This reverts commit ae0483ef

--- src/main/java/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {                           // [1]
-    public static void main(String[] args) {         // [2]
-        System.out.println("Hello World!"); // [3]
-        System.out.println("Ewa"); // [4]
-        System.out.println(36); // [5]
-        System.out.println("D"); // [6]
-    }
-}
