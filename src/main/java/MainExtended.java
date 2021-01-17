b226108ac35b39b96d285b895070ac8d82176c81
Ewa Dzioba <ewa.dzioba@gmail.com>
2021-01-17 10:40:10 +0100

    Wersja ostateczna do sprawdzenia

--- src/main/java/MainExtended.java
+++ src/main/java/MainExtended.java
@@ -1,16 +1,16 @@
 public class MainExtended {
     public static void main(String[] args) {
-        String mySurname = "Grażyna";
+        String secondName = "Grażyna";
         int myShoe = 38;
         double age = 35.5;
-        char favouriteLetter = 'E';
+        char exclamationMark = '!';
         boolean isHappy = true;
         String myGreetings = "Hello from FirstClass!";
-        System.out.println(mySurname);
+        System.out.println(secondName);
         System.out.println(myShoe);
         System.out.println(age);
         System.out.println(myGreetings);
-        System.out.println(favouriteLetter);
+        System.out.println(exclamationMark);
         System.out.println(isHappy);
     }
 }
